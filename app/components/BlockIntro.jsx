663a144f36cf0cb218e432a24d9650225e5d63e6
FernandoJC <fernandojcastellanos@gmail.com>
2023-10-13 11:25:40 -0400

    promo fix

--- app/components/BlockIntro.jsx
+++ app/components/BlockIntro.jsx
@@ -27,7 +27,7 @@ const BlockIntro = () => {
       </div>
       <div className=" w-[100%] sm:w-[50%] text-center relative flex justify-center items-center" onClick={toggleVideo}>
         <Image className={`absolute w-[15%] h-[15%] z-20 ${videoPlayer ? 'hidden' : 'visible'}`} src={arrow} alt="arrow" width={1000} height={1000} />
-        <Image className={`absolute w-[100%] h-[100%] object-cover z-10 ${videoPlayer ? 'hidden' : 'visible'}`} alt="VideoImage" src={promo} />
+        <Image className={`absolute w-[100%] h-[100%] px-5 bg-black z-10 ${videoPlayer ? 'hidden' : 'visible'}`} alt="VideoImage" src={promo} />
         <iframe className={`h-[350px] w-[95%] `}  width="100%" height="100%" src="https://www.youtube.com/embed/Pd3ikFsSNVM?si=zA9A4ugClU1hbWOv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
       </div>
     </div>
